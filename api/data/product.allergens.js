7fa33fe9e74a76ec1e0d0a9770a7bdf8d4ef1c43
Luis Eduardo Urdaneta Martucci <LuisEUM95@gmail.com>
2022-10-04 19:42:50 +0200

    Models Category, Product and Restaurant

---
+++ api/data/product.allergens.js
@@ -0,0 +1,58 @@
+const allergens = [
+  {
+    value: "celery",
+    label: "Celery",
+    image: "/images/allergens/Celery.png",
+  },
+  { value: "nuts", label: "Nuts", image: "/images/allergens/Nuts.png" },
+  {
+    value: "Mustard",
+    label: "Mustard",
+    image: "/images/allergens/Mustard.png",
+  },
+  { value: "eggs", label: "Eggs", image: "/images/allergens/Eggs.png" },
+  { value: "soy", label: "Soy", image: "/images/allergens/Soy.png" },
+  {
+    value: "sesameSeed",
+    label: "Sesame Seed",
+    image: "/images/allergens/Sesame_Seed.png",
+  },
+  {
+    value: "peanuts",
+    label: "Peanuts",
+    image: "/images/allergens/Peanuts.png",
+  },
+  {
+    value: "gluten",
+    label: "Gluten",
+    image: "/images/allergens/Gluten.png",
+  },
+  {
+    value: "crustacean",
+    label: "Crustacean",
+    image: "/images/allergens/Crustacean.png",
+  },
+  { value: "fish", label: "Fish", image: "/images/allergens/Fish.png" },
+  {
+    value: "sulphites",
+    label: "Sulphites",
+    image: "/images/allergens/Sulphites.png",
+  },
+  {
+    value: "dairyProducts",
+    label: "Dairy Products",
+    image: "/images/allergens/Dairy_Products.png",
+  },
+  {
+    value: "lupinBean",
+    label: "Lupin Bean",
+    image: "/images/allergens/Lupin_Bean.png",
+  },
+  {
+    value: "mollusks",
+    label: "Mollusks",
+    image: "/images/allergens/Mollusks.png",
+  },
+];
+
+module.exports = allergens;
