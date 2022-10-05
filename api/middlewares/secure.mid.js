8298d92d733b5ca69a96711f7bc20271d739daf9
Pedro Salazar <38982098+pedrofran@users.noreply.github.com>
2022-10-05 10:36:57 +0200

    restaurant owned middleware

--- api/middlewares/secure.mid.js
+++ api/middlewares/secure.mid.js
@@ -1,4 +1,5 @@
 const createError = require('http-errors');
+const Restaurant = require('../models/restaurant.model');
 
 module.exports.isAuthenticated = (req, res, next) => {
   if (req.user) {
@@ -7,3 +8,19 @@ module.exports.isAuthenticated = (req, res, next) => {
     next(createError(401))
   }
 }
+
+module.exports.restaurantIsOwnedByUser = (req, res, next) => {
+  const { id } = req.params;
+  Restaurant.findById(id)
+    .then((restaurant) => {
+      if (restaurant?.user == req.user?.id) {
+        req.restaurant = restaurant;
+        next();
+      } else if (restaurant) {
+        next(createError(403, "Nooooo puedeeeees paasaaaaar"));
+      } else {
+        next(createError(404, "restaurant not found"));
+      }
+    })
+    .catch(next);
+};
